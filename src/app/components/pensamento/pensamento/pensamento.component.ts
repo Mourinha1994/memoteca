b6f6dacb12ec8e466ce887885c49f071290e108b
Erick Moura <erikmelodemoura17@gmail.com>
2024-09-07 19:17:41 -0300

    feat: pensamento component

--- src/app/components/pensamento/pensamento/pensamento.component.ts
+++ src/app/components/pensamento/pensamento/pensamento.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pensamento',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pensamento.component.html',
   styleUrl: './pensamento.component.css'
 })
 export class PensamentoComponent {
 
+  @Input() pensamento: any;
+
+  larguraPensamento(): string {
+    if (this.pensamento.conteudo.length > 256) {
+      return 'pensamento-g'
+    }
+
+    return 'pensamento-p'
+  }
 }
