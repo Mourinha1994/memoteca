81cc6815a0a0e95e38cf7f8eb965faaf47bac565
Erick Moura <erikmelodemoura17@gmail.com>
2024-09-07 15:32:17 -0300

    feat: criado componente form pensamentos

---
+++ src/app/components/pensamentos/form-pensamentos/form-pensamentos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-pensamentos',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-pensamentos.component.html',
+  styleUrl: './form-pensamentos.component.css'
+})
+export class FormPensamentosComponent {
+
+}
