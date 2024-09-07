0c520ea6dd38cbeadd2f184d7f3ec3e2dcf12f0a
Erick Moura <erikmelodemoura17@gmail.com>
2024-09-07 16:54:10 -0300

    feat: criado componente de pensamento

---
+++ src/app/components/pensamento/pensamento/pensamento.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  standalone: true,
+  imports: [],
+  templateUrl: './pensamento.component.html',
+  styleUrl: './pensamento.component.css'
+})
+export class PensamentoComponent {
+
+}
