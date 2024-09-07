8e8e0d529096127404838ad309d8921cdb8ba1bd
Erick Moura <erikmelodemoura17@gmail.com>
2024-09-07 16:13:38 -0300

    feat: adicionando two-way data binding no formulário

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from "./components/cabecalho/cabecalho.component";
 import { RodapeComponent } from './components/rodape/rodape.component';
 import { FormPensamentosComponent } from "./components/pensamentos/form-pensamentos/form-pensamentos.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CabecalhoComponent, RodapeComponent, FormPensamentosComponent],
+  imports: [
+    RouterOutlet, 
+    CabecalhoComponent, 
+    RodapeComponent, 
+    FormPensamentosComponent, 
+    CommonModule, 
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/pensamentos/form-pensamentos/form-pensamentos.component.ts
+++ src/app/components/pensamentos/form-pensamentos/form-pensamentos.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-pensamentos',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './form-pensamentos.component.html',
   styleUrl: './form-pensamentos.component.css'
 })
 export class FormPensamentosComponent {
+  pensamento = {
+    id: 1,
+    conteudo: '',
+    autoria: ''
+  }
 
+  criarPensamento() {
+    alert("Pensamento criado")
+  }
+
+  cancelar() {
+    alert("Acão cancelada")
+  }
 }
